c60947cfd21a1fbe02e463a54bb20d3309d16338
Per <pers_1957@hotmail.com>
2016-02-24 11:27:47 +0100

    Init

---
+++ LMS_grupp1/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_grupp1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Personnumber { get; set; }
+        public RoleType Role { get; set; }
+
+        public virtual IEnumerable<RoleType> Roles { get; set; }
+    }
+}
