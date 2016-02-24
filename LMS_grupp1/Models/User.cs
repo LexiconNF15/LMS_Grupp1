40d91c48fc812434666e03e5fad49ef3ef9bb0fa
Per <pers_1957@hotmail.com>
2016-02-24 11:34:03 +0100

    removed roles model

--- LMS_grupp1/Models/User.cs
+++ LMS_grupp1/Models/User.cs
@@ -10,8 +10,5 @@ namespace LMS_grupp1.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Personnumber { get; set; }
-        public RoleType Role { get; set; }
-
-        public virtual IEnumerable<RoleType> Roles { get; set; }
     }
 }
