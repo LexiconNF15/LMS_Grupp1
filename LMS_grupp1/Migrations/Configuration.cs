2e5d2e97fff2ea03004a1d1cb0531d34816c2b55
Per <pers_1957@hotmail.com>
2016-02-26 14:04:16 +0100

    restore seed

--- LMS_grupp1/Migrations/Configuration.cs
+++ LMS_grupp1/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace LMS_grupp1.Migrations
 {
     using LMS_grupp1.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -30,27 +32,71 @@ namespace LMS_grupp1.Migrations
             //    );
             //
             CultureInfo provider = CultureInfo.InvariantCulture;
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            foreach (string roleName in new[] { "Teacher", "Student" })
+            {
+                if (!context.Roles.Any(r => r.Name == roleName))
+                {
+                    var role = new IdentityRole { Name = roleName };
+                    roleManager.Create(role);
+                }
+            }
+
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+
+            var seedUsers = new[] { 
+                new {Email = "Kalle@Urbom.se", Role= "Teacher"},
+                new {Email = "Nils@Persson.se", Role= "Student"},
+                new {Email = "Alice@Eriksson.se", Role= "Student"},
+                new {Email = "Anna@Ulfsdotter.se", Role= "Student"},
+                new {Email = "Orvar@Framsteg.se", Role= "Student"},
+                new {Email = "Tore@Pettersson.se", Role= "Teacher"}
+
+            
+            };
+            foreach (var seedUser in seedUsers)
+                if (!context.Users.Any(u => u.UserName == seedUser.Email))
+                {
+                    var user = new ApplicationUser { UserName = seedUser.Email, Email = seedUser.Email };
+                    var result = userManager.Create(user, "foobar");
+                    //var roleUser = userManager.FindByName(seedUser.Email);
+                    if (result.Succeeded) userManager.AddToRole(user.Id, seedUser.Role);
+                }
+
+            //var teacherUser = userManager.FindByName("Kalle@Urbom.se");
+            //userManager.AddToRole(teacherUser.Id, "Teacher");
+
+            //var studentUser = userManager.FindByName("Nils@Persson.se");
+            //userManager.AddToRole(studentUser.Id, "Student");
+
+
             context.Groups.AddOrUpdate(
-                 p => p.Name,
-                  new Group
-                  {
-                      Name = ".NET November",
-                      StartTime = DateTime.ParseExact("11/23/2016", "d", provider),
-                      EndTime = DateTime.ParseExact("03/18/2016", "d", provider)
-                  },
-                  new Group
-                  {
-                      Name = "Java Februari",
-                      StartTime = DateTime.ParseExact("02/23/2016", "d", provider),
-                      EndTime = DateTime.ParseExact("05/18/2016", "d", provider)
-                  },
-                  new Group
-                  {
-                      Name = ".NET September",
-                      StartTime = DateTime.ParseExact("08/23/2016", "d", provider),
-                      EndTime = DateTime.ParseExact("01/18/2016", "d", provider)
-                  }
-            );
+                p => p.Name,
+                 new Group
+                 {
+                     Name = ".NET November",
+                     StartTime = DateTime.ParseExact("11/23/2016", "d", provider),
+                     EndTime = DateTime.ParseExact("03/18/2016", "d", provider)
+                 },
+                 new Group
+                 {
+                     Name = "Java Februari",
+                     StartTime = DateTime.ParseExact("02/23/2016", "d", provider),
+                     EndTime = DateTime.ParseExact("05/18/2016", "d", provider)
+                 },
+                 new Group
+                 {
+                     Name = ".NET September",
+                     StartTime = DateTime.ParseExact("08/23/2016", "d", provider),
+                     EndTime = DateTime.ParseExact("01/18/2016", "d", provider)
+                 }
+);
         }
     }
 }
