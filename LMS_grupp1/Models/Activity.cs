8b6d496b6b2d148e70eaf0aca04f077a07fa15f0
mouhannad1986 <mouhannad1986@windowslive.com>
2016-03-02 11:10:18 +0100

    Adding Activities Controller and View

---
+++ LMS_grupp1/Models/Activity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace LMS_grupp1.Models
+{
+    public class Activity
+    {
+
+        public int Id { get; set; }
+
+        [DisplayName("Benämning")]
+        public string Name { get; set; }
+
+        [DisplayName("Beskrivning")]
+        public string Description { get; set; }
+
+        [DisplayName("Startdatum")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime StartTime { get; set; }
+
+        [DisplayName("Slutdatum")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime EndTime { get; set; }
+
+        [DisplayName("Kurs Namn")]
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+
+    }
+}
